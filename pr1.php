815abd8eb5d3ff3ea450254b7edce19c8752b5b4
Chudini03 <125361608+Chudini03@users.noreply.github.com>
2023-02-23 12:30:12 +0100

    Add files via upload

--- pr1.php
+++ pr1.php
@@ -17,7 +17,7 @@
 	</header>
 	
 	<main>
-		<section>
+		
 			<h2>Oferty</h2>
 
 		<div class="kon">
@@ -27,15 +27,29 @@ $polaczenie=mysqli_connect("localhost","zset_spychala","Spychala_123","zset_spyc
 $wyswietlenie=mysqli_query($polaczenie,"SELECT * FROM hotel");
 while($r=mysqli_fetch_row($wyswietlenie)){
 	
-	for ($i = 0; $i < $r[5]; $i++)
+	
 
 	echo"<div class='okienko'>";
-	echo"<div class='foto'> <img style='width:100%;height:100%;'src='obraz.jpeg' alt='Foto'></div>";
+	echo"<div class='foto'> <img style='width:100%;height:100%;'src='zdjecia/$r[7]' alt='Foto'></div>";
 	echo"<div class='info'>";
-	echo"<div class='title'>$r[1]  $g</div>";
+	echo"<div class='title'>$r[1]";
+	for ($i = 0; $i < $r[5]; $i++){
+		echo "<img src='gwiazdka.png'>";
+	}
+	
+	
+	echo"</div>";
+	
 	echo"<div class='kraj'>$r[2]</div>";
 	echo"<div class='wylot'>Poznań</div>";
-	echo"<div class='jedzenie'>Brak jedzenia</div>";
+	echo"<div>";
+	for ($i = 0; $i < $r[3]; $i++){
+		echo "<img src='zdjecia/human.png'>";
+	}
+	echo"</div>";
+	echo"<div class='jedzenie'>Czy jest jedzenie?:$r[4]</br>
+	rozrywka:$r[6]
+	</div>";
 	echo"</div>";
 	echo"<div class='cena'> <b>1495zł<b></div>";
 	echo"</div>";
@@ -45,30 +59,23 @@ mysqli_close($polaczenie);
 ?>
 </div>
 		
-			<!--<div class="offer">
-				<img src="obraz.jpeg" alt="Oferta 1">
-				<h3>Wakacje w Egipcie</h3>
-				<p>Cena od 2000 zł za osobę</p>
-				<a href="#" class="button">Zobacz ofertę</a>
-			</div>
-			<div class="offer">
-				<img src="bg.png" alt="Oferta 2">
-				<h3>Zwiedzanie Rzymu</h3>
-				<p>Cena od 2500 zł za osobę</p>
-				<a href="#" class="button">Zobacz ofertę</a>
-			</div>
-			<div class="offer">
-				<img src="https://via.placeholder.com/400x250" alt="Oferta 3">
-				<h3>Wakacje w Chorwacji</h3>
-				<p>Cena od 1500 zł za osobę</p>
-				<a href="#" class="button">Zobacz ofertę</a>
-			</div>-->
-		</section>
-		
+
+
+
+
+
+<div id="infoblock2">
+    <div id="infohalf1"> <d style="font-size: 30px;">O nas </d><br> <br>
+	<p>Jesteśmy biurem podróży, które od kilku lat organizuje wycieczki do najpiękniejszych miejsc na świecie. Nasza oferta jest bardzo szeroka, dzięki czemu każdy znajdzie coś dla siebie. Zawsze stawiamy na jakość usług oraz zadowolenie naszych klientów.</p>
+			<p>Jeśli szukasz wymarzonej wycieczki, to trafiłeś w dobre miejsce! Skontaktuj się z nami, a my pomożemy Ci zorganizować niezapomniane wakacje.</p>        
+
+</div>
+    <div id="infohalf2"> <img style= "width: 90%; height: 400px;" src="ludek.svg" > </div>
+    <div style="clear: both;"></div>
+</div>
 		<section>
-			<h2>O nas</h2>
-			<p>Jesteśmy biurem podróży, które od kilku lat organizuje wycieczki do najpiękniejszych miejsc na świecie. Nasza oferta jest bardzo szeroka, dzięki czemu każdy znajdzie coś dla siebie. Zawsze stawiamy na jakość usług oraz zadowolenie naszych klientów.</p>
-			<p>Jeśli szukasz wymarzonej wycieczki, to trafiłeś w dobre miejsce! Skontaktuj się z nami, a my pomożemy Ci zorganizować niezapomniane wakacje.</p>
+			<h2></h2>
+			
 		</section>
 		
 		<section>

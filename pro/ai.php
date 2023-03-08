3444e9e10fafff953f52b7a6448b2a1d9f3c47f4
Chudini03 <125361608+Chudini03@users.noreply.github.com>
2023-03-08 15:15:49 +0100

    Add files via upload

---
+++ pro/ai.php
@@ -0,0 +1,70 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="UTF-8">
+	<title>Biuro Podróży - Twoja Wymarzona Wycieczka</title>
+	<link rel="stylesheet" type="text/css" href="style1.css">
+</head>
+<body>
+	<header>
+		<nav>
+			<ul>
+				<li><a href="#">Strona Główna</a></li>
+				<li><a href="#">Oferty</a></li>
+				<li><a href="#">Kontakt</a></li>
+			</ul>
+		</nav>
+		<h1>Biuro Podróży</h1>
+	</header>
+
+	<main>
+		<section class="hero">
+			<h2>Twoja Wymarzona Wycieczka</h2>
+			<p>Zaplanuj swój urlop z nami!</p>
+			<button>Zobacz Oferty</button>
+		</section>
+
+		<section class="featured-offers">
+			<h2>Nasze Najpopularniejsze Oferty</h2>
+			<div class="offer">
+				<img src="https://via.placeholder.com/300x200" alt="Oferta 1">
+				<h3>Wakacje na Malediwach</h3>
+				<p>Cena od: $3000</p>
+				<button>Sprawdź szczegóły</button>
+			</div>
+			<div class="offer">
+				<img src="https://via.placeholder.com/300x200" alt="Oferta 2">
+				<h3>Wycieczka do Egiptu</h3>
+				<p>Cena od: $1500</p>
+				<button>Sprawdź szczegóły</button>
+			</div>
+			<div class="offer">
+				<img src="https://via.placeholder.com/300x200" alt="Oferta 3">
+				<h3>Zwiedzanie Włoch</h3>
+				<p>Cena od: $2000</p>
+				<button>Sprawdź szczegóły</button>
+			</div>
+		</section>
+
+		<section class="testimonials">
+			<h2>Opinie Naszych Klientów</h2>
+			<div class="testimonial">
+				<blockquote>"Wakacje na Malediwach były najlepszym wypoczynkiem w moim życiu!"</blockquote>
+				<p>- Jan Kowalski</p>
+			</div>
+			<div class="testimonial">
+				<blockquote>"Dzięki Biuru Podróży nie musiałem się martwić o żadne szczegóły, wakacje w Egipcie były świetne!"</blockquote>
+				<p>- Maria Nowak</p>
+			</div>
+			<div class="testimonial">
+				<blockquote>"Zwiedzanie Włoch z przewodnikiem z Biura Podróży było niezapomniane!"</blockquote>
+				<p>- Paweł Zieliński</p>
+			</div>
+		</section>
+	</main>
+
+	<footer>
+		<p>&copy; 2023 Biuro Podróży</p>
+	</footer>
+</body>
+</html>
---
+++ pro/dis.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style2.css">
+    <title>SCROL</title>
+</head>
+<body>
+<header>
+		<div id=logo>
+			<img src="logo.png" alt="tu bedzie logo">
+		</div>
+		<div>
+			<ul>
+				<li><a href="#">Home</a></li>
+				<li><a href="#">Oferty</a></li>
+				<li><a href="#">O nas</a></li>
+				<li><a href="#">Kontakt</a></li>
+			</ul>
+		</div>
+	</header>
+    <div class="scroll-con">
+        <div class="scroll-area">
+            <img src="zdjecia/spain.png" alt="">
+        </div>
+        <div class="scroll-area">
+            <img src="zdjecia/2.jpg" alt="">
+        </div>
+        <div class="scroll-area">
+            <img src="zdjecia/3.jpg" alt="">
+        </div>
+    </div>
+</body>
+</html>
